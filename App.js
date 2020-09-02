1b8e5b3dbbc78116a012752ec8f38dc9325d434d
samuel929 <samlambo929@gmail.com>
2020-09-02 07:40:20 +0200

    Added a text input and a button and change the state of the textInput

--- App.js
+++ App.js
@@ -1,21 +1,63 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,TextInput,Button } from 'react-native';
 
-export default function App() {
+export default class App extends React.Component {
+  constructor(){
+    super()
+      this.state={
+        userInput:'',
+        goals:[]
+      }
+    
+  }
+
+  goalInputHandler=text=>{
+    this.setState({
+      userInput:text
+    })
+  }
+
+  addGoalHandler=()=>{
+
+  }
+
+  render(){
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+       <View style={styles.InputContainer}>
+           <TextInput 
+            placeholder="Enter a Goal" 
+            style={styles.input}
+            onChangeText={this.goalInputHandler}
+            value={this.state.userInput}
+            />
+          <Button 
+            title="Add" 
+            color="purple"
+            onPress={this.addGoalHandler}
+            />
+       </View>
+       <View>
+
+       </View>
     </View>
   );
 }
+}
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container:{
+    padding:50
   },
+    InputContainer:{
+    flexDirection:'row',
+    justifyContent:'space-between',
+    alignItems:'center'
+  },
+    input:{borderColor:'black',
+    borderWidth:1,
+    padding:10,
+    width:'80%'
+    }
 });
